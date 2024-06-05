13790af81406d91030b6a06081a8951d67fb454b
bogach816777 <70478971+bogach816777@users.noreply.github.com>
2024-06-05 15:58:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 3000;
 // Налаштування CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Дозволяємо доступ з будь-якого джерела
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Дозволяємо різні методи запитів
+    res.setHeader('Access-Control-Allow-Methods', 'GET'); // Дозволяємо різні методи запитів
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Дозволяємо вказувати заголовки
     next();
 });
